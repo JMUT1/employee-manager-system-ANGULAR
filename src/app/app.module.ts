23da7707f9eb9a32466283750e70244ee5f616be
Chema Torres <chemaaa94@gmail.com>
2022-12-05 15:37:53 -0600

    creation of different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmployeeManagerComponent } from './component/employee-manager/employee-manager.component';
 import { HeaderComponent } from './component/header/header.component';
+import { ManagerSystemComponent } from './component/employee-manager/manager-system/manager-system.component';
+import { SystemListComponent } from './component/employee-manager/manager-system/system-list/system-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeManagerComponent,
-    HeaderComponent
+    HeaderComponent,
+    ManagerSystemComponent,
+    SystemListComponent,
   ],
   imports: [
     BrowserModule
