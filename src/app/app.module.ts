d9d12869e9208617d87a6980a90de62cb1680b6d
Chema Torres <chemaaa94@gmail.com>
2022-12-05 15:10:25 -0600

    creating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeSystemComponent } from './employee-system/employee-system.component';
+import { EmployeeManagerComponent } from './component/employee-manager/employee-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeSystemComponent,
+    EmployeeManagerComponent
   ],
   imports: [
     BrowserModule
