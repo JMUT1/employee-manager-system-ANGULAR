b2b0f7042732f64326cb3c3852a608b8d8c204aa
Chema Torres <chemaaa94@gmail.com>
2022-12-05 15:16:58 -0600

    create header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EmployeeSystemComponent } from './employee-system/employee-system.component';
 import { EmployeeManagerComponent } from './component/employee-manager/employee-manager.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeSystemComponent,
-    EmployeeManagerComponent
+    EmployeeManagerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
